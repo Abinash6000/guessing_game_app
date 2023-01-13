c61eabfb44d9ace2e8d0f0ec2c4c2902b4048017
Abinash <abinashdash6000@gmail.com>
2023-01-14 03:49:57 +0530

    Update

--- app/src/main/java/com/project/guessinggame/GameFragment.kt
+++ app/src/main/java/com/project/guessinggame/GameFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.navigation.findNavController
 import com.project.guessinggame.databinding.FragmentGameBinding
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
 
 class GameFragment : Fragment() {
     private var _binding: FragmentGameBinding? = null
@@ -20,18 +21,30 @@ class GameFragment : Fragment() {
     ): View? {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         val view = binding.root
-        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
-        updateScreen()
+        viewModel = ViewModelProvider(this)[GameViewModel::class.java]
+
+        viewModel.incorrectGuesses.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.incorrectGuesses.text = "Incorrect guesses: $newValue"
+        })
+
+        viewModel.livesLeft.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.lives.text = "You have $newValue lives left"
+        })
+
+        viewModel.secretWordDisplay.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.word.text = newValue
+        })
+
+        viewModel.gameOver.observe(viewLifecycleOwner, Observer { newValue ->
+            if (newValue) {
+                val action = GameFragmentDirections.actionGameFragmentToResultFragment(viewModel.wonLostMessage())
+                view.findNavController().navigate(action)
+            }
+        })
 
         binding.guessButton.setOnClickListener {
             viewModel.makeGuess(binding.guess.text.toString().uppercase())
             binding.guess.text = null
-            updateScreen()
-            if (viewModel.isWon() || viewModel.isLost()) {
-                val action = GameFragmentDirections
-                    .actionGameFragmentToResultFragment(viewModel.wonLostMessage())
-                view.findNavController().navigate(action)
-            }
         }
         return view
     }
@@ -39,11 +52,5 @@ class GameFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    fun updateScreen() {
-        binding.word.text = viewModel.secretWordDisplay
-        binding.lives.text = "You have ${viewModel.livesLeft} lives left."
-        binding.incorrectGuesses.text = "Incorrect guesses: ${viewModel.incorrectGuesses}"
     }
 }
